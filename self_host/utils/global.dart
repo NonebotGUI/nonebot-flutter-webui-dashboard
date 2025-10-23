7134141b44af78af400a75389d6b3b005dc71ab4
XTxiaoting14332 <HT30332@126.com>
2025-10-23 14:40:10 +0800

    🏷️v0.1.12+2

--- self_host/utils/global.dart
+++ self_host/utils/global.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 late WebSocket socketToAgent;
 late WebSocket socketToClient;
 int wsStatus = 0;
-String version = '0.1.12+1';
+String version = '0.1.12+2';
 
 /// 用户配置
 class Config {
