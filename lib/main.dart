c7dda0f8f65768617972614b77720f3b260dd68c
XTxiaoting14332 <HT30332@126.com>
2025-10-30 19:48:25 +0800

    🏷️v0.1.13+1

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:flutter/services.dart' show rootBundle;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  version = '0.1.13';
+  version = '0.1.13+1';
   debug = false;
 
   String initialThemeMode = 'light';
