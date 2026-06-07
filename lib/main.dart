8b448bec444052fb950bd976cfd7dba8fbe1940b
XTxiaoting14332 <HT30332@126.com>
2026-06-07 14:52:25 +0800

    🏷️v0.1.19

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:flutter/services.dart' show rootBundle;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  version = '0.1.18';
+  version = '0.1.19';
   debug = false;
 
   String initialThemeMode = 'light';
--- self_host/utils/global.dart
+++ self_host/utils/global.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 late WebSocket socketToAgent;
 late WebSocket socketToClient;
 int wsStatus = 0;
-String version = '0.1.18';
+String version = '0.1.19';
 
 /// 用户配置
 class Config {
